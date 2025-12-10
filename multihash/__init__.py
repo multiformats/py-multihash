37bbfa26e5066d8a499f174c240b5baa6cba79a8
sumanjeet0012@gmail.com <sumanjeet0012@gmail.com>
2025-12-11 00:59:13 +0530

    Add type annotations for Multihash fields in __init__.py

--- multihash/__init__.py
+++ multihash/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for py-multihash."""
 
+from typing import TYPE_CHECKING
+
 __author__ = """Dhruv Baldawa"""
 __email__ = "dhruv@dhruvb.com"
 __version__ = "2.0.0"
@@ -31,6 +33,15 @@ from .multihash import (
     to_b58_string,
     to_hex_string,
 )
+
+# Explicitly expose types for static type checkers
+if TYPE_CHECKING:
+    from .funcs import Func as Func
+    from .funcs import FuncReg as FuncReg
+    from .funcs import IdentityHash as IdentityHash
+    from .funcs import ShakeHash as ShakeHash
+    from .multihash import Multihash as Multihash
+    from .multihash import MultihashSet as MultihashSet
 
 __all__ = [
     "Func",
